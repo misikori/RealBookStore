fc0e1ad9871dd10f64f9b4ec35974d7d24988107
Misikori <obrenovicmilos01@gmail.com>
2024-04-28 03:46:40 +0200

    authorization

--- src/main/java/com/urosdragojevic/realbookstore/controller/BooksController.java
+++ src/main/java/com/urosdragojevic/realbookstore/controller/BooksController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -31,6 +32,7 @@ public class BooksController {
     private PersonRepository personRepository;
 
     @GetMapping({"/", "/books"})
+    @PreAuthorize("hasAuthority('VIEW_BOOKS_LIST')")
     public String home(Model model) {
         model.addAttribute("books", bookRepository.getAll());
         return "books";
@@ -68,18 +70,21 @@ public class BooksController {
     }
 
     @GetMapping("/create-form")
+    @PreAuthorize("hasAuthority('CREATE_BOOK')")
     public String CreateForm(Model model) {
         model.addAttribute("genres", genreRepository.getAll());
         return "create-form";
     }
 
     @GetMapping(value = "/books/search", produces = "application/json")
+    @PreAuthorize("hasAuthority('VIEW_BOOKS_LIST')")
     @ResponseBody
     public List<Book> search(@RequestParam("query") String query) throws SQLException {
         return bookRepository.search(query);
     }
 
     @PostMapping("/books")
+    @PreAuthorize("hasAuthority('CREATE_BOOK')")
     public String createBook(NewBook book) {
         List<Genre> genreList = this.genreRepository.getAll();
         List<Genre> genresToInsert = book.getGenres().stream().map(bookGenreId -> genreList.stream().filter(genre -> genre.getId() == bookGenreId).findFirst().get()).collect(Collectors.toList());
